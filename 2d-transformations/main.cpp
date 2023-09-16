9ef26b9d955c641ee646e50b7327c98e9358ae42
gaurovgiri <gaurovgiri@gmail.com>
2023-09-16 22:48:11 +0545

    Add makefile and matrix headers

--- 2d-transformations/main.cpp
+++ 2d-transformations/main.cpp
@@ -1,14 +1,14 @@
 #include <vector>
 #include <iostream>
 #include "utils.hpp"
+#include "transformations.hpp"
 
 using namespace std;
 
 int main()
 {
-    vector<vector<int>> matrix1= {{0,1,0},{1,0,1},{1,1,1}};
-    vector<vector<int>> matrix2= {{0,1,0},{1,0,1},{1,1,1}};
-    vector<vector<int>> result = matmul(matrix1,matrix2);
-    printMat(result);
+    vector<vector<int>> matrix= {{0,1,0},{1,0,1},{1,1,1}};
+    vector<vector<int>> translatedMatrix = matmul(translation(2,5),matrix);
+    printMat(translatedMatrix);
     return 0;
 }
---
+++ 2d-transformations/matrix.hpp
@@ -0,0 +1,31 @@
+#ifndef __MATRIX__
+#define __MATRIX__
+
+#include <vector>
+#include <iostream>
+
+class Matrix
+{
+private:
+    int rows;
+    int cols;
+    std::vector<std::vector<int>> mat;
+
+public:
+    Matrix(const std::vector<std::vector<int>> data);
+    Matrix(const int rows, const int cols);
+    
+    Matrix operator+(const Matrix& other);
+    Matrix operator*(const Matrix& other);
+    Matrix operator-(const Matrix& other);
+
+
+    friend std::iostream operator<<(std::ostream& os, const Matrix& matrix);
+
+    int rows();
+    int cols();
+
+    
+    ~Matrix();
+};
+#endif // __MATRIX__
